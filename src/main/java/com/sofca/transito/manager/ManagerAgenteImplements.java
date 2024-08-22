f39cf9f52ec22876164cca4a09ab8ec1bddc349a
Jose Barrios <josealfredobarriossalado@gmail.com>
2024-08-21 20:55:20 -0500

    dependencia de suagger

--- src/main/java/com/sofca/transito/manager/ManagerAgenteImplements.java
+++ src/main/java/com/sofca/transito/manager/ManagerAgenteImplements.java
@@ -31,6 +31,7 @@ public class ManagerAgenteImplements implements ManagerAgenteInterface{
 
     @Override
     public void delete(AgenteDTO agenteDTO) {
+
         this.agenteDaoInterface.delete(agenteDTO);
     }
 
